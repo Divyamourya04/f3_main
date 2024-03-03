c9dc33306f9248b1fcd5d34eb02d42e105b7815e
Divyamourya04 <116952899+Divyamourya04@users.noreply.github.com>
2024-03-03 22:00:06 +0530

    first commit and uploaded files

---
+++ app.js
@@ -0,0 +1,19 @@
+ let a = prompt("hello");
+function Info(name){
+    console.log('my name is ${this.name} and i co')
+}
+ const person1={
+  name:"coder",
+    about:Info();
+}
+const person2={
+    name:"girls",
+    about:Info();
+
+}
+person1.about();
+person2.about();
+
+
+
+
